ee8ff630ef9b01648d8061c1b22e43ed4e0488c0
Sibgha-Zeeshan <132210204+Sibgha-Zeeshan@users.noreply.github.com>
2024-07-22 02:29:20 +0500

    updating some routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
 from backend.app.routers import users, activitylog, emissionfactor, goals, achievements, report, tips
 from backend.app.database import engine, Base
+import sys
+import os
+
+# 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# CORS 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
+#Origins of frontend and backend
+origins = [
+    #URL of frontend
+    #domain of backend from ngrok
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include the routers
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(activitylog.router, prefix="/activity-logs", tags=["activity_logs"]) 
